0313a248b323133873c2491d60e53221f17668d4
Lodi <lodi.creates@gmail.com>
2022-04-05 12:00:17 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    // ARGUMENT VALIDITY //
+    if (int1 < 0 || int2 < 0) {return `ERROR`};
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) {return `ERROR`};
+    
+    // LOCAL VARIABLES //
+    let result = 0;  // declare new variable to store result and initialize to 0
 
+    // FUNCTION //
+    for (let i = Math.min(int1, int2); i <= Math.max(int1, int2); i++){  // iterate through numbers from smaller integer to larger integer and add each to result
+        result += i;
+    };
+    return result;  // return result
 };
 
 // Do not edit below this line
