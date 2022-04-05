bd20bddcd9ede8d4b55819326f705a049bee5718
Lodi <lodi.creates@gmail.com>
2022-04-05 12:31:24 -0400

    Complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) {  // if divisible by 400, leap year
+        return true;
+    } else if (year % 100 === 0) {  // not divisible by 400 but divisible by 100, not leap year
+        return false;
+    } else if (year % 4 === 0) {  // not divisible by 400 or 100, but divisible by 4, leap year
+        return true;
+    } else {  // otherwise, not leap year
+        return false;
+    };
 };
 
 // Do not edit below this line
