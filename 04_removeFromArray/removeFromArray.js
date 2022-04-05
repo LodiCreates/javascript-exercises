4d1d72bd88be7cde28b19087a9fb0059d155e128
Lodi <lodi.creates@gmail.com>
2022-04-05 11:17:44 -0400

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    let resultArray = [];  // create resultArray to store result
+    arrayLoop: for (const item of array) {  // iterate through each item in array
+        for (const itemToRemove of itemsToRemove) {  // check if item matches any itemsToRemove
+            if (item === itemToRemove) {
+                continue arrayLoop;  // if match, move to next item in array without adding current item to resultArray
+            };
+        };
+        resultArray.push(item);  // if no matches, push current item into resultArray
+    };
+    return resultArray;
 };
 
 // Do not edit below this line
