9aaa991e69f5c2f99ad7893e44230f47edd56f06
Lodi <lodi.creates@gmail.com>
2022-04-05 12:51:16 -0400

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let result;  // declare variable to store result in
+  result = (fahrenheit - 32) * (5 / 9);  // subtract 32 and multiply by 5/9
+  result = +result.toFixed(1);  // round to first decimal
+  return result;  // return result
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let result;  // declare variable to store result in
+  result = celsius * (9 / 5) + 32;  // multiply by 9/5 and add 32
+  result = +result.toFixed(1);  // round to first decimal
+  return result;  // return result
 };
 
 // Do not edit below this line
