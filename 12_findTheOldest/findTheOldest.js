3180c0d94cf400a182e66f8952d3e5d784f70a24
Lodi <lodi.creates@gmail.com>
2022-04-28 10:25:42 -0400

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((currentOldest,nextPerson) => {
+        // get ages
+        let currentOldestAge;
+        if (currentOldest.yearOfDeath === undefined) {
+            currentOldestAge = ((new Date()).getFullYear()) - currentOldest.yearOfBirth;
+        } else {
+            currentOldestAge = currentOldest.yearOfDeath - currentOldest.yearOfBirth;
+        };
+        let nextPersonAge;
+        if (nextPerson.yearOfDeath === undefined) {
+            nextPersonAge = ((new Date()).getFullYear()) - nextPerson.yearOfBirth;
+        } else {
+            nextPersonAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        };
+        // compare ages
+        return (currentOldestAge > nextPersonAge) ? currentOldest : nextPerson;
+    }, peopleArray[0]);
 };
 
 // Do not edit below this line
