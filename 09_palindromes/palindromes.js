e879a19c2a5f96b0edcc9d42ea020d8da3440dd9
Lodi <lodi.creates@gmail.com>
2022-04-28 09:08:01 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // get forward string of only lowercase letters
+    let lettersForward = string
+                                .replace(/[^A-Za-z0-9]/g,'')
+                                .toLowerCase();
+    // get reverse string of only lowercase letters
+    let lettersReverse = lettersForward
+                                        .split('')
+                                        .reverse()
+                                        .join('');
+    // compare
+    return lettersForward === lettersReverse;
 };
 
 // Do not edit below this line
